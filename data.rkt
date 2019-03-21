939c8ed263eb061d741b1db54310b9ebbeabb705
Shaobo <polarishehn@gmail.com>
2019-03-20 18:13:58 -0600

    added fp->bv

--- data.rkt
+++ data.rkt
@@ -20,8 +20,8 @@
 (define BitVec->bits
   (λ (bv)
     (let ([v (BitVec-value bv)])
-      (for/list ([p (range (BitVec-width bv))])
-        (if (bitwise-bit-set? v p) 1 0)))))
+      (reverse (for/list ([p (range (BitVec-width bv))])
+        (if (bitwise-bit-set? v p) 1 0))))))
 
 (define eval/id
   (curry BitVec-value))
@@ -153,6 +153,14 @@
                 ,(FloatingPoint-sig-width fp)
                 ,(FloatingPoint-value fp)))))])
 
+(define fp/infinity?
+  (λ (fp)
+    (bfinfinite? (FloatingPoint-value fp))))
+
+(define fp/nan?
+  (λ (fp)
+    (bfnan? (FloatingPoint-value fp))))
+
 (define real->FloatingPoint
   (λ (rv exp-width sig-width)
     (parameterize ([bf-precision sig-width])
@@ -160,27 +168,57 @@
 
 (define BitVec->FloatingPoint
   (λ (bv exp-width sig-width)
-    (if (= (BitVec-width bv) (+ exp-width sig-width))
-        (parameterize ([bf-precision sig-width])
-          (let* ([bv-value (BitVec-value bv)]
-                 [sig-width-wo (- sig-width 1)]
-                 [sig-bits (modulo bv-value (expt 2 sig-width-wo))]
-                 [exp-bits (modulo (arithmetic-shift bv-value (- 0 sig-width-wo))
-                                   (expt 2 exp-width))]
-                 [exp-bias (- (expt 2 (- exp-width 1)) 1)]
-                 [sign-bit (bitwise-bit-set? bv-value (+ exp-width sig-width-wo))])
-            (cond
-              [(= (- (expt 2 exp-width) 1) exp-bits) ; exp all 1s
-               (if (= sig-bits 0)
-                   (bfcopy (if sign-bit -inf.bf +inf.bf))
-                   (bfcopy +nan.bf))]
-              [(= exp-bits 0) ; exp all 0s
-               (if (= sig-bits 0)
-                   (bfcopy (if sign-bit -0.bf 0.bf))
-                   (bf (if sign-bit (- 0 sig-bits) sig-bits)
-                       (- (+ (- 0 exp-bias) 1) sig-width-wo)))]
-              [else (bf (let ([sig (+ (expt 2 sig-width-wo) sig-bits)])
-                          (if sign-bit (- 0 sig) sig))
-                        (- (- exp-bits exp-bias)
-                           sig-width-wo))])))
-        (error "Bit width doesn't match!"))))
+    (mkFP exp-width sig-width
+          (if (= (BitVec-width bv) (+ exp-width sig-width))
+              (parameterize ([bf-precision sig-width])
+                (let* ([bv-value (BitVec-value bv)]
+                       [sig-width-wo (- sig-width 1)]
+                       [sig-bits (modulo bv-value (expt 2 sig-width-wo))]
+                       [exp-bits (modulo (arithmetic-shift bv-value (- 0 sig-width-wo))
+                                         (expt 2 exp-width))]
+                       [exp-bias (- (expt 2 (- exp-width 1)) 1)]
+                       [sign-bit (bitwise-bit-set? bv-value (+ exp-width sig-width-wo))])
+                  (cond
+                    [(= (- (expt 2 exp-width) 1) exp-bits) ; exp all 1s
+                     (if (= sig-bits 0)
+                         (bfcopy (if sign-bit -inf.bf +inf.bf))
+                         (bfcopy +nan.bf))]
+                    [(= exp-bits 0) ; exp all 0s
+                     (if (= sig-bits 0)
+                         (bfcopy (if sign-bit -0.bf 0.bf))
+                         (bf (if sign-bit (- 0 sig-bits) sig-bits)
+                             (- (+ (- 0 exp-bias) 1) sig-width-wo)))]
+                    [else (bf (let ([sig (+ (expt 2 sig-width-wo) sig-bits)])
+                                (if sign-bit (- 0 sig) sig))
+                              (- (- exp-bits exp-bias)
+                                 sig-width-wo))])))
+              (error "Bit width doesn't match!")))))
+
+(define FloatingPoint->BitVec
+  (λ (fp)
+    (let* ([exp-width (FloatingPoint-exp-width fp)]
+           [sig-width (FloatingPoint-sig-width fp)]
+           [fp-val (FloatingPoint-value fp)]
+           [sig-width-wo (- sig-width 1)]
+           [exp-bias (- (expt 2 (- exp-width 1)) 1)]
+           [sign-wrap (λ (v)
+                        (if (bfnegative? fp-val)
+                            (+ v (expt 2 (+ exp-width sig-width-wo)))
+                            v))])
+      (mkBV
+       (+ exp-width sig-width)
+       (cond
+         [(fp/nan? fp) (error "no unique bv representation for nans!")]
+         [(fp/infinity? fp) (sign-wrap (arithmetic-shift (- (expt 2 exp-width) 1) sig-width-wo))]
+         [else
+          (let-values ([(sig exp) (bigfloat->sig+exp fp-val)])
+            (sign-wrap
+             (if (and (>= sig (expt 2 sig-width-wo))
+                      (< sig (expt 2 sig-width)))
+                 (if (>= exp (- (- 1 exp-bias) sig-width-wo)) ; normals
+                     (+
+                      (arithmetic-shift (- exp (- (- 0 exp-bias) sig-width)) (- sig-width-wo 1))
+                      (modulo sig (expt 2 (- sig-width 1))))
+                     (/ sig (expt 2 (- (- 1 (+ exp-bias sig-width-wo)) exp))))
+                 (error "unrecognized format for sig+exp!"))))])))))
+
