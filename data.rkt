a8c1cd1f185631ddcc7dc9dceeca11a507d13a9f
Shaobo <polarishehn@gmail.com>
2019-03-21 15:48:02 -0600

    removed unnecessary usage of curry

--- data.rkt
+++ data.rkt
@@ -28,42 +28,33 @@
 
 ; arithmetic operations
 ; binary
-(define eval/arith/binop
-  (λ (op bv1 bv2)
-    (let ([r (op (BitVec-value bv1)
-                 (BitVec-value bv2))]
-          [d (expt 2 (BitVec-width bv1))])
-      (mkBV (BitVec-width bv1)
-            (modulo r d)))))
+(define ((eval/arith/binop op) bv1 bv2)
+  (let ([r (op (BitVec-value bv1)
+               (BitVec-value bv2))]
+        [d (expt 2 (BitVec-width bv1))])
+    (mkBV (BitVec-width bv1)
+          (modulo r d))))
 
-(define bv-pred
-  (λ (op bv1 bv2)
-    (if (= (BitVec-width bv1)
-           (BitVec-width bv2))
-        (op (BitVec-value bv1)
-            (BitVec-value bv2))
-        (error "not the same bit-width"))))
+(define ((bv-pred op) bv1 bv2)
+  (if (= (BitVec-width bv1)
+         (BitVec-width bv2))
+      (op (BitVec-value bv1)
+          (BitVec-value bv2))
+      (error "not the same bit-width")))
 
-(define bv=
-  ((curry bv-pred) =))
+(define bv= (bv-pred =))
 
-(define bv<
-  ((curry bv-pred) <))
+(define bv< (bv-pred <))
 
-(define bv≤
-  ((curry bv-pred) <=))
+(define bv≤ (bv-pred <=))
 
-(define eval/bvadd
-  ((curry eval/arith/binop) +))
+(define eval/bvadd (eval/arith/binop +))
 
-(define eval/bvsub
-  ((curry eval/arith/binop) -))
+(define eval/bvsub (eval/arith/binop -))
 
-(define eval/bvmul
-  ((curry eval/arith/binop) *))
+(define eval/bvmul (eval/arith/binop *))
 
-(define eval/bvudiv
-  ((curry eval/arith/binop) /))
+(define eval/bvudiv (eval/arith/binop /))
 
 (define eval/bvurem
   (λ (bv1 bv2)
@@ -80,16 +71,15 @@
 
 ; bitwise operations
 ; binary
-(define eval/bitwise/binop
-  (λ (op bv1 bv2)
-    (mkBV (BitVec-width bv1) (op (BitVec-value bv1)
-        (BitVec-value bv2)))))
+(define ((eval/bitwise/binop op) bv1 bv2)
+  (mkBV
+   (BitVec-width bv1)
+   (op (BitVec-value bv1)
+       (BitVec-value bv2))))
 
-(define eval/bvand
-  ((curry eval/bitwise/binop bitwise-and)))
+(define eval/bvand (eval/bitwise/binop bitwise-and))
 
-(define eval/bvor
-  ((curry eval/bitwise/binop bitwise-ior)))
+(define eval/bvor (eval/bitwise/binop bitwise-ior))
 
 (define eval/bvshl
   (λ (bv1 bv2)
