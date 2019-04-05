a7c9a28ed6aac02c94e005f49bb0a9a56208f99d
Shaobo <polarishehn@gmail.com>
2019-04-04 18:22:42 -0600

    avoided re-computing scores naively
    partially addressed #6

--- sls.rkt
+++ sls.rkt
@@ -14,50 +14,11 @@
       [(struct FloatingPoint _) (get/fp-extended-neighbors v)]
       [_ (error "unimplemented type!")])))
 
-(define select/Candidates
-  ;;(λ (F assignment selected moves c2)
-  (λ (F assignment wp c2)
-    (let* ([currScore ((score c2 assignment) F)]
-           [candAssertion (select/Assertion F assignment c2)]
-           [get/neighbors
-            (λ (candVars)
-              (apply append
-                     (map
-                      (λ (candVar)
-                        (let ([bv (get-value assignment candVar)])
-                          (map ((curry update/Assignment) assignment candVar)
-                               (get/extended-neighbors bv))))
-                      candVars)))]
-           [select/Move
-            (λ (candVars)
-              (let ([neighbors (get/neighbors candVars)])
-                (if (coin-flip wp)
-                    (cons #t (list-ref neighbors (random (length neighbors))))
-                    (cons #f (argmax
-                     (λ (a) ((score c2 a) F))
-                     neighbors)))))])
-      (let ([local-opt (select/Move (get/vars candAssertion assignment))])
-        (if (car local-opt)
-            (cons #t (cdr local-opt))
-        (if (> ((score c2 (cdr local-opt)) F) currScore)
-            (cons #t (cdr local-opt))
-            (cons #f (cdr local-opt))))))))
-
-(define select/Assertion
-  ;;(λ (F assignment selected moves c2)
-  (λ (F assignment c2)
-    ;; assume when the score of an assertion is 1, it's satisfiable
-    ;; no diversification TODO: UCB
-    (let ([as (get/assertions F)])
-      (cdr (argmax (λ (t) (let ([s ((score c2 assignment) (cdr t))])
-                            (if (< s 1) s -1)))
-                   (for/list ([i (in-range (length as))]
-                              [a as])
-                     (cons i a)))))))
-
+#|
 (define isSat?
   (λ (F assignment c2)
     (= ((score c2 assignment) F) 1.0)))
+|#
 
 (define update/Assignment
   (λ (assignment sym val)
@@ -141,19 +102,61 @@
   (λ (var-info F c2 maxSteps wp)
     (define sls/do
       (λ (i assignment)
-        (if (>= i maxSteps)
-            (begin
-              (displayln assignment)
-              'unknown)
-            (if (isSat? F assignment c2)
-                (begin
-                  (displayln i)
-                  assignment)
-                (let ([newAssign (select/Candidates F assignment wp c2)])
-                  (if (car newAssign)
-                      (sls/do (+ i 1) (cdr newAssign))
-                      (sls/do (+ i 1) (randomAssign var-info (cdr newAssign) i))
-                      ))))))
+        (define select/Candidates
+          ;;(λ (F assignment selected moves c2)
+          (λ (assert-scores)
+            (let* ([currScore (/ (apply + assert-scores) (length assert-scores))]
+                   [candAssertion (select/Assertion assert-scores)]
+                   [get/neighbors
+                    (λ (candVars)
+                      (apply append
+                             (map
+                              (λ (candVar)
+                                (let ([bv (get-value assignment candVar)])
+                                  (map ((curry update/Assignment) assignment candVar)
+                                       (get/extended-neighbors bv))))
+                              candVars)))]
+                   [select/Move
+                    (λ (candVars)
+                      (let ([neighbors (get/neighbors candVars)])
+                        (if (coin-flip wp)
+                            (cons #t (list-ref neighbors (random (length neighbors))))
+                            (cons #f (argmax
+                                      (λ (a) ((score c2 a) F))
+                                      neighbors)))))])
+              (let ([local-opt (select/Move (get/vars candAssertion assignment))])
+                (if (car local-opt)
+                    (cons #t (cdr local-opt))
+                    (if (> ((score c2 (cdr local-opt)) F) currScore)
+                        (cons #t (cdr local-opt))
+                        (cons #f (cdr local-opt))))))))
+        (define select/Assertion
+          ;;(λ (F assignment selected moves c2)
+          (λ (assert-scores)
+            ;; assume when the score of an assertion is 1, it's satisfiable
+            ;; no diversification TODO: UCB
+            (let ([asserts (get/assertions F)])
+              (cdr (argmax (λ (t) (if (< (car t) 1) (car t) -1))
+                           (for/list ([as assert-scores]
+                                      [a asserts])
+                             (cons as a)))))))
+        (cond
+          [(>= i maxSteps)
+           (begin
+             (displayln assignment)
+             'unknown)]
+          [else (let* ([asserts (get/assertions F)]
+                       [assert-scores (map (score c2 assignment) asserts)])
+                 ;(if (isSat? F assignment c2)
+                  (if (andmap (λ (s) (= s 1)) assert-scores)
+                    (begin
+                      (displayln i)
+                      assignment)
+                    (let ([newAssign (select/Candidates assert-scores)])
+                      (if (car newAssign)
+                          (sls/do (+ i 1) (cdr newAssign))
+                          (sls/do (+ i 1) (randomAssign var-info (cdr newAssign) i))
+                          ))))])))
     (sls/do 0 (initialize/Assignment var-info))))
 
 ;(define assignment (hash-set (hash-set (make-immutable-hash) "x" (mkBV 8 100)) "y" (mkBV 8 50)))
