93cb817cf1e321dc9773b15b89fba55cc5c559ca
Shaobo He <polarishehn@gmail.com>
2019-04-04 18:21:12 -0600

    added main module
    fixed #7

---
+++ main.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(require "sls.rkt"
+         "parsing/parse.rkt"
+         "parsing/transform.rkt")
+
+(define main
+  (λ ()
+    (define args (current-command-line-arguments))
+    (let* ([script (file->sexp (vector-ref args 0))]
+           [formula (remove-fpconst
+                     (simplify
+                      (unnest
+                       (formula->nnf
+                        (remove-let-bindings
+                         (get-formula script))))))]
+           [var-info (get-var-info script)]
+           [seed (string->number (vector-ref args 1))])
+      (begin
+        (random-seed seed)
+        (sls var-info formula 1/2 100 0.0001)))))
+
+(main)
