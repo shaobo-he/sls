001ca8b5c9565afdba7836a560990603216dac59
Shaobo He <polarishehn@gmail.com>
2019-04-08 17:42:15 -0600

    better arguments

--- main.rkt
+++ main.rkt
@@ -4,22 +4,71 @@
 
 (require "sls.rkt"
          "parsing/parse.rkt"
-         "parsing/transform.rkt")
+         "parsing/transform.rkt"
+         racket/cmdline)
+
+(define seed (make-parameter 1))
+(define c2 (make-parameter 1/2))
+(define wp (make-parameter 0.001))
+(define step (make-parameter 200))
+
+(define file-to-analyze
+  (command-line
+   #:program "sls"
+   #:once-each
+   [("--seed") seed-arg
+               "RNG seed"
+               (define n-seed (string->number seed-arg))
+               (if (and
+                    n-seed
+                    (exact-positive-integer? n-seed)
+                    (>= n-seed 0)
+                    (<= n-seed (sub1 (expt 2 31))))
+                   (seed n-seed)
+                   (error "not a valid seed"))]
+   [("--c2") c2-arg
+             "Score scaling constant"
+             (define n-c2 (string->number c2-arg))
+             (if (and
+                  n-c2
+                  (rational? n-c2)
+                  (>= n-c2 0)
+                  (<= n-c2 1))
+                 (c2 n-c2)
+                 (error "not a valid score scaling constant"))]
+   [("--wp") wp-arg
+             "Diversification probability"
+             (define n-wp (string->number wp-arg))
+             (if (and
+                  n-wp
+                  (>= n-wp 0)
+                  (<= n-wp 1))
+                 (wp n-wp)
+                 (error "not a valid diversification probability"))]
+   [("--step") step-arg
+               "Search steps"
+               (define n-step (string->number step-arg))
+               (if (and
+                    n-step
+                    (exact-nonnegative-integer? n-step))
+                   (step n-step)
+                   (error "not a valid search step"))]
+   #:args (filename) ; expect one command-line argument: <filename>
+   ; return the argument as a filename to compile
+   filename))
 
 (define main
   (λ ()
-    (define args (current-command-line-arguments))
-    (let* ([script (file->sexp (vector-ref args 0))]
+    (let* ([script (file->sexp file-to-analyze)]
            [formula (remove-fpconst
                      (simplify
                       (unnest
                        (formula->nnf
                         (remove-let-bindings
                          (get-formula script))))))]
-           [var-info (get-var-info script)]
-           [seed (string->number (vector-ref args 1))])
+           [var-info (get-var-info script)])
       (begin
-        (random-seed seed)
-        (sls var-info formula 1/2 200 0.0001)))))
+        (random-seed (seed))
+        (sls var-info formula (c2) (step) (wp))))))
 
 (main)
