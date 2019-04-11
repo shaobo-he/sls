372cf543ad049aefa1434271e8d3d025b90ecf4d
Shaobo <polarishehn@gmail.com>
2019-04-11 12:46:52 -0600

    seal the state

--- main.rkt
+++ main.rkt
@@ -7,63 +7,61 @@
          "parsing/transform.rkt"
          racket/cmdline)
 
-(define seed (make-parameter 1))
-(define c2 (make-parameter 1/2))
-(define wp (make-parameter 0.001))
-(define step (make-parameter 200))
-(define start-with-zeros? (make-parameter #t))
-
-(define file-to-analyze
-  (command-line
-   #:program "sls"
-   #:once-each
-   ["--seed" seed-arg
-               "RNG seed"
-               (define n-seed (string->number seed-arg))
-               (if (and
-                    n-seed
-                    (exact-positive-integer? n-seed)
-                    (>= n-seed 0)
-                    (<= n-seed (sub1 (expt 2 31))))
-                   (seed n-seed)
-                   (error "not a valid seed"))]
-   ["--c2" c2-arg
-             "Score scaling constant"
-             (define n-c2 (string->number c2-arg))
-             (if (and
-                  n-c2
-                  (rational? n-c2)
-                  (>= n-c2 0)
-                  (<= n-c2 1))
-                 (c2 n-c2)
-                 (error "not a valid score scaling constant"))]
-   ["--wp" wp-arg
-             "Diversification probability"
-             (define n-wp (string->number wp-arg))
-             (if (and
-                  n-wp
-                  (>= n-wp 0)
-                  (<= n-wp 1))
-                 (wp n-wp)
-                 (error "not a valid diversification probability"))]
-   ["--step" step-arg
-               "Search steps"
-               (define n-step (string->number step-arg))
-               (if (and
-                    n-step
-                    (exact-nonnegative-integer? n-step))
-                   (step n-step)
-                   (error "not a valid search step"))]
-   ["--initialize-with-random" ("Initialize search space with random values"
-                                "otherwise search space is initialized to all 0s")
-                               (start-with-zeros? #f)]
-   #:args (filename) ; expect one command-line argument: <filename>
-   ; return the argument as a filename to compile
-   filename))
-
 (define main
   (λ ()
-    (let* ([script (file->sexp file-to-analyze)]
+    (let* ([seed (make-parameter 1)]
+           [c2 (make-parameter 1/2)]
+           [wp (make-parameter 0.001)]
+           [step (make-parameter 200)]
+           [start-with-zeros? (make-parameter #t)]
+           [file-to-analyze
+            (command-line
+             #:program "sls"
+             #:once-each
+             ["--seed" seed-arg
+                       "RNG seed"
+                       (define n-seed (string->number seed-arg))
+                       (if (and
+                            n-seed
+                            (exact-positive-integer? n-seed)
+                            (>= n-seed 0)
+                            (<= n-seed (sub1 (expt 2 31))))
+                           (seed n-seed)
+                           (error "not a valid seed"))]
+             ["--c2" c2-arg
+                     "Score scaling constant"
+                     (define n-c2 (string->number c2-arg))
+                     (if (and
+                          n-c2
+                          (rational? n-c2)
+                          (>= n-c2 0)
+                          (<= n-c2 1))
+                         (c2 n-c2)
+                         (error "not a valid score scaling constant"))]
+             ["--wp" wp-arg
+                     "Diversification probability"
+                     (define n-wp (string->number wp-arg))
+                     (if (and
+                          n-wp
+                          (>= n-wp 0)
+                          (<= n-wp 1))
+                         (wp n-wp)
+                         (error "not a valid diversification probability"))]
+             ["--step" step-arg
+                       "Search steps"
+                       (define n-step (string->number step-arg))
+                       (if (and
+                            n-step
+                            (exact-nonnegative-integer? n-step))
+                           (step n-step)
+                           (error "not a valid search step"))]
+             ["--initialize-with-random" ("Initialize search space with random values"
+                                          "otherwise search space is initialized to all 0s")
+                                         (start-with-zeros? #f)]
+             #:args (filename) ; expect one command-line argument: <filename>
+             ; return the argument as a filename to compile
+             filename)]
+           [script (file->sexp file-to-analyze)]
            [formula (remove-fpconst
                      (simplify
                       (unnest
